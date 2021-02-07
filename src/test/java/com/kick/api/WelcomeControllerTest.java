c98d74e25da3eb24964b23da24455fb19bd53a1f
엄태준 <>
2021-02-07 21:26:12 +0900

    테스트 클래스 추가

--- src/test/java/com/kick/api/WelcomeControllerTest.java
+++ src/test/java/com/kick/api/WelcomeControllerTest.java
@@ -1,30 +1,18 @@
 package com.kick.api;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kick.api.vo.BoardVo;
 
 @ExtendWith(SpringExtension.class) 
 @SpringBootTest 
 @AutoConfigureMockMvc
 class WelcomeControllerTest {
-	@Autowired 
-	private MockMvc mockMvc;
+//	@Autowired 
+//	private MockMvc mockMvc;
 	
 	
 //	private static ObjectMapper objectMapper = new ObjectMapper(); 
