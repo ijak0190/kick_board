da0c25707b07100ece23a7ef93cd3b155e46164e
엄태준 <>
2021-02-07 21:39:45 +0900

    테스트 케이스 추가

--- src/test/java/com/kick/api/WelcomeControllerTest.java
+++ src/test/java/com/kick/api/WelcomeControllerTest.java
@@ -1,23 +1,30 @@
 package com.kick.api;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 @ExtendWith(SpringExtension.class) 
 @SpringBootTest 
 @AutoConfigureMockMvc
 class WelcomeControllerTest {
-//	@Autowired 
-//	private MockMvc mockMvc;
+	@Autowired 
+	private MockMvc mockMvc;
 	
 	
 //	private static ObjectMapper objectMapper = new ObjectMapper(); 
 	
-//	final BoardVo boardVo = BoardVo.builder().brdIdx(0).brdSbj("테스").build(); 
+//	final BoardVo boardVo = BoardVo.builder().brdIdx(0).brdSbj("test").build(); 
 	 
 //	@BeforeEach 
 //	public void setup() throws Exception { 
@@ -28,7 +35,7 @@ class WelcomeControllerTest {
 	public void testGetUsers() throws Exception { 
 		// when 
 		// then 
-//		this.mockMvc.perform(get("/boardList").contentType(MediaType.APPLICATION_JSON)) .andExpect(status().isOk()).andDo(print()); 
+		this.mockMvc.perform(get("/boardList").contentType(MediaType.APPLICATION_JSON)) .andExpect(status().isOk()).andDo(print()); 
 		String value = "안녕";
 		assertEquals(value,"안녕");
 	}
